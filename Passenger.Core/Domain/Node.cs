a59d91d4cd70c47c224384625d43b778a6abb5bc
MartinOgon <marcin.ogonowski@gmail.com>
2020-11-09 15:18:25 +0100

    ctor for Node with validation + Async Driver

--- Passenger.Core/Domain/Node.cs
+++ Passenger.Core/Domain/Node.cs
@@ -1,10 +1,15 @@
+using System;
+using System.Text.RegularExpressions;
+
 namespace Passenger.Core.Domain
 {
     public class Node
     {
+        private static readonly Regex NameRegex = new Regex("^(?![_.-])(?!.*[_.-]{2})[a-zA-Z0-9._.-]+(?<![_.-])$");
         public string Address{ get; protected set; }
         public double Longitude{ get; protected set; }
         public double Latitude{ get; protected set; }
+        public DateTime UpdatedAt { get; protected set; }
 
         protected Node()
         {
@@ -12,10 +17,50 @@ namespace Passenger.Core.Domain
 
         public Node(string address, double longitute, double latitude)
         {
-            Address = address;
-            Longitude = longitute;
-            Latitude = latitude;
+            SetAddress(address);
+            SetLongitude(longitute);
+            SetLatitude(latitude);
         }
 
+        private void SetLatitude(double latitude)
+        {
+            if(!double.IsNaN(latitude))
+            {
+                throw new Exception("Please enter valid value.");
+            }
+            if (Latitude == latitude)
+            {
+                return;
+            }
+            Latitude = latitude;
+            UpdatedAt = DateTime.UtcNow;
+        }
+
+        private void SetLongitude(double longitute)
+        {
+            if(!double.IsNaN(longitute))
+            {
+                throw new Exception("Please enter valid value.");
+            }
+            if (Longitude == longitute)
+            {
+                return;
+            }
+            Longitude = longitute;
+            UpdatedAt = DateTime.UtcNow;
+        }
+
+        private void SetAddress(string address)
+        {
+            if (!NameRegex.IsMatch(address))
+            {
+                throw new Exception ("Address is invalid.");
+            }
+            Address = address;
+            UpdatedAt = DateTime.UtcNow;
+        }
+
+        public static Node Create(string address, double longitute, double latitude)
+            => new Node(address, longitute, latitude);
     }
 }
--- Passenger.Infrastructure/Services/DriverService.cs
+++ Passenger.Infrastructure/Services/DriverService.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Threading.Tasks;
+using AutoMapper;
 using Passenger.Core.Domain;
 using Passenger.Core.Repositories;
 using Passenger.Infrastructure.DTO;
@@ -8,19 +10,18 @@ namespace Passenger.Infrastructure.Services
     public class DriverService : IDriverService
     {
         private readonly IDriverRepository _driverRepository;
+        private readonly IMapper _mapper;
 
-        public DriverService(IDriverRepository driverRepository)
+        public DriverService(IDriverRepository driverRepository, IMapper mapper)
         {
             _driverRepository = driverRepository;
+            _mapper = mapper;
         }
 
-        public DriverDto Get(Guid userId)
+        public async Task<DriverDto> GetAsync(Guid userId)
         {
-            var driver = _driverRepository.Get(userId);
-            return new DriverDto
-            {
-                //id
-            };
+            var driver = await _driverRepository.GetAsync(userId);
+            return _mapper.Map<Driver, DriverDto>(driver);
         }
     }
 }
